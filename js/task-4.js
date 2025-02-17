c95dae66bf030325e9c541205aa2590c58d1b5a0
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-02-17 22:52:36 +0200

    feat: hw7

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+const loginForm = document.querySelector(".login-form");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+}
+
+loginForm.addEventListener("submit", handleSubmit);
---
+++ js/task-6.js
@@ -0,0 +1,42 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+const createBtn = document.querySelector("button[data-create]");
+
+function createBoxes(amount) {
+  if (isNaN(amount) || amount < 1 || amount > 100) {
+    alert("Please enter a number between 1 and 100");
+    return;
+  }
+
+  boxes.replaceChildren();
+
+  for (let i = 0; i < amount; i += 1) {
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${30 + i * 10}px`;
+    newDiv.style.height = `${30 + i * 10}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+
+    boxes.appendChild(newDiv);
+  }
+
+  input.value = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  createBoxes(amount);
+});
+
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+function destroyBoxes() {
+  boxes.replaceChildren();
+}
+
+destroyBtn.addEventListener("click", destroyBoxes);
